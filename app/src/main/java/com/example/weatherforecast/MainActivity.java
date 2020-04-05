24ef8a0b2eb071c99ca63a22571dd6e38235793f
alismac <alialaee98@gmail.com>
2020-04-06 01:27:10 +0430

    JSON files are correctly read and ready to use in RecyclerView - Handler added - Worker thread sends the query to the API and the answer is handled and ready in strings.

--- app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,18 +12,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
     private static final String TAG = "MainActivity";
     private Button button_start_thread;
-    private TextView textView; //for testing the server response
     private EditText editText; //edit text that contains the input text
     private String json_url = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+    private RequestQueue mQueue;
+    private Handler mainHandler = new Handler();
 
 
     @Override
@@ -32,16 +39,14 @@ public class MainActivity extends AppCompatActivity {
 
         button_start_thread = findViewById(R.id.go_btn);
         editText = findViewById(R.id.inputText);
+        mQueue = Volley.newRequestQueue(this);
 
         button_start_thread.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                json_url += editText.getText();     // text written in the edit text
-                json_url += ".json?access_token=";  // finnishing the url
-                json_url += "pk.eyJ1IjoiYWxpYXNoOTgiLCJhIjoiY2s4bjdmZHk4MG13bTNmcGU1c3ZmdmZudiJ9.WyMiEFZqNFeI2hxx68CHhg"; //token
-                // string is now ready to use
 
-                startActivity(new Intent(MainActivity.this, SecondPageAcitivity.class));
+
+//               startActivity(new Intent(MainActivity.this, SecondPageAcitivity.class));
 
                 GoRunnable goRunnable = new GoRunnable();
                 Thread goThread = new Thread(goRunnable);
@@ -53,33 +58,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+    //Thread that handles the communication with MapBox API
     class GoRunnable implements Runnable{
         @Override
         public void run() {
+            json_url += editText.getText();     // text written in the edit text
+            json_url += ".json?access_token=";  // finnishing the url
+            json_url += "pk.eyJ1IjoiYWxpYXNoOTgiLCJhIjoiY2s4bjdmZHk4MG13bTNmcGU1c3ZmdmZudiJ9.WyMiEFZqNFeI2hxx68CHhg"; //token
+            // string is now ready to use
+
             // Sending the query to the API
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, json_url, null, new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("features");
+                        for (int i = 0 ; i < jsonArray.length() ; i++){
+                            JSONObject temp = jsonArray.getJSONObject(i);
+                            final String place = temp.getString("place_name");
+                            String coordination = temp.getString("center");
 
+                            mainHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                // the city name and coordination should get into the recyclerView
+                                }
+                            });
+
+                            try {
+                                Thread.sleep(1500);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+//                            Log.d(TAG, place);
+//                            Log.d(TAG, coordination);
+                        }
+                    }catch (JSONException e){
+                        e.printStackTrace();
+                    }
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-
+                    error.printStackTrace();
                 }
             });
+            mQueue.add(jsonObjectRequest);
 
-
-
-            for (int i = 0 ; i < 15 ; i++){
-                try{
-                    Thread.sleep(1500);
-                    Log.d(TAG, "run: " + i);
-                } catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
