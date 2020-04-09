e8c68ab31892807e183d150235d2a7f286beb879
Kiarash Golzadeh <kiarash.gl@gmail.com>
2020-04-09 23:47:04 +0430

    Store data in json

--- app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -32,115 +32,111 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-    private Button button_start_thread;
-    private EditText editText; //edit text that contains the input text
-//    private String json_url;
-    private RequestQueue mQueue;
-    private Handler mainHandler = new Handler();
-    private ListView myListView;    //it shows the cities found
-    private ArrayList<String> cityItems = new ArrayList<String>();
-    private ArrayList<String> cityXY = new ArrayList<String>();
+	private static final String TAG = "MainActivity";
+	private Button button_start_thread;
+	private EditText editText; //edit text that contains the input text
+	//    private String json_url;
+	private RequestQueue mQueue;
+	private Handler mainHandler = new Handler();
+	private ListView myListView;    //it shows the cities found
+	private ArrayList<String> cityItems = new ArrayList<String>();
+	private ArrayList<String> cityXY = new ArrayList<String>();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        // check internet connection
-        if (isNetworkConnected(MainActivity.this)){
-            button_start_thread = findViewById(R.id.go_btn);
-            editText = findViewById(R.id.inputText);
-            mQueue = Volley.newRequestQueue(this);
-            myListView = findViewById(R.id.listview);
-
-            button_start_thread.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
 
-//              startActivity(new Intent(MainActivity.this, SecondPageActivity.class));
-                    GoRunnable goRunnable = new GoRunnable();
-                    Thread goThread = new Thread(goRunnable);
-                    goThread.start();
-                }
-            });
-        }
-        else {
-            // here we should go to the second activity
+		// check internet connection
+		button_start_thread = findViewById(R.id.go_btn);
+		editText = findViewById(R.id.inputText);
+		mQueue = Volley.newRequestQueue(this);
+		myListView = findViewById(R.id.listview);
 
-           // Toast.makeText(MainActivity.this, "No Internet", Toast.LENGTH_SHORT).show();
-        }
+		button_start_thread.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (isNetworkConnected(MainActivity.this)) {
+					GoRunnable goRunnable = new GoRunnable();
+					Thread goThread = new Thread(goRunnable);
+					goThread.start();
+				} else {
+					Toast.makeText(MainActivity.this, "No Internet", Toast.LENGTH_SHORT).show();
+					Intent intent = new Intent(MainActivity.this, SecondPageActivity.class);
+					intent.putExtra("loadPrevious", true);
+					MainActivity.this.startActivity(intent);
+				}
+			}
+		});
+	}
 
-    }
+	//Thread that handles the communication with MapBox API
+	class GoRunnable implements Runnable {
+		@Override
+		public void run() {
+			String json_url = getString(R.string.mapbox_api_url, editText.getText());
+			// text written in the edit text
+			// string is now ready to use
+			cityItems.clear();
+			cityXY.clear();
+			// Sending the query to the API
 
-    //Thread that handles the communication with MapBox API
-    class GoRunnable implements Runnable{
-        @Override
-        public void run() {
-            String json_url = getString(R.string.mapbox_api_url, editText.getText());
-            // text written in the edit text
-            // string is now ready to use
-            cityItems.clear();
-            cityXY.clear();
-            // Sending the query to the API
-
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, json_url, null, new Response.Listener<JSONObject>() {
-                @Override
-                public void onResponse(JSONObject response) {
-                    try {
-                        JSONArray jsonArray = response.getJSONArray("features");
-                        cityItems.clear();
-                        cityXY.clear();
-                        for (int i = 0 ; i < jsonArray.length() ; i++){
-                            JSONObject temp = jsonArray.getJSONObject(i);
-                            String place = temp.getString("place_name");
-                            String XY = temp.getString("center");
-                            cityItems.add(place);
-                            cityXY.add(XY);
-                        }
-                    }catch (JSONException e){
-                        e.printStackTrace();
-                    }
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    error.printStackTrace();
-                }
-            });
-            mQueue.add(jsonObjectRequest);
-            mainHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
-                            android.R.layout.simple_list_item_1, cityItems);
-                    myListView.setAdapter(adapter);
-                    myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                            Intent intent = new Intent(MainActivity.this, SecondPageActivity.class);
-                            intent.putExtra("XY", cityXY.get(position));
-                            MainActivity.this.startActivity(intent);
-                        }
-                    });
-                    // the city name and coordination gets into the ListView
-                }
-            });
+			JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, json_url, null, new Response.Listener<JSONObject>() {
+				@Override
+				public void onResponse(JSONObject response) {
+					try {
+						JSONArray jsonArray = response.getJSONArray("features");
+						cityItems.clear();
+						cityXY.clear();
+						for (int i = 0; i < jsonArray.length(); i++) {
+							JSONObject temp = jsonArray.getJSONObject(i);
+							String place = temp.getString("place_name");
+							String XY = temp.getString("center");
+							cityItems.add(place);
+							cityXY.add(XY);
+						}
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				}
+			}, new Response.ErrorListener() {
+				@Override
+				public void onErrorResponse(VolleyError error) {
+					error.printStackTrace();
+				}
+			});
+			mQueue.add(jsonObjectRequest);
+			mainHandler.post(new Runnable() {
+				@Override
+				public void run() {
+					ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+							android.R.layout.simple_list_item_1, cityItems);
+					myListView.setAdapter(adapter);
+					myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+						@Override
+						public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+							Intent intent = new Intent(MainActivity.this, SecondPageActivity.class);
+							intent.putExtra("XY", cityXY.get(position));
+							intent.putExtra("loadPrevious", false);
+							MainActivity.this.startActivity(intent);
+						}
+					});
+				}
+			});
 
 
-        }
-    }
+		}
+	}
 
-    private boolean isNetworkConnected(Context context) {
-        ConnectivityManager cm =
-                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+	private boolean isNetworkConnected(Context context) {
+		ConnectivityManager cm =
+				(ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        boolean isConnected = activeNetwork != null &&
-                activeNetwork.isConnectedOrConnecting();
-        return isConnected;
-    }
+		NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+		boolean isConnected = activeNetwork != null &&
+				activeNetwork.isConnectedOrConnecting();
+		return isConnected;
+	}
 
 }
--- app/src/main/java/com/example/weatherforecast/SecondPageActivity.java
+++ app/src/main/java/com/example/weatherforecast/SecondPageActivity.java
@@ -6,12 +6,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.JsonReader;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -22,31 +20,70 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 public class SecondPageActivity extends AppCompatActivity {
+	class WeatherData
+	{
+		private WeatherCondition currentCondition;
+		private WeatherCondition[] forecastedConditions;
+		private String name, region, country;
+		public WeatherData(String name, String region, String country, WeatherCondition currentCondition, WeatherCondition[] forecastedConditions) {
+			this.currentCondition = currentCondition;
+			this.forecastedConditions = forecastedConditions;
+			this.name = name;
+			this.region = region;
+			this.country = country;
+		}
+
+		public WeatherCondition getCurrentCondition() {
+			return currentCondition;
+		}
+
+		public WeatherCondition[] getForecastedConditions() {
+			return forecastedConditions;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getRegion() {
+			return region;
+		}
+
+		public String getCountry() {
+			return country;
+		}
+	}
 	private RequestQueue mQueue;
 	private Handler mainHandler = new Handler();
 	private String longitude;
 	private String latitude;
 	private String name, region, country;
 	private WeatherCondition currentCondition;
-	private final int NUMBER_OF_DAYS = 7;
+	private static final int NUMBER_OF_DAYS = 7;
 	private RecyclerView recyclerView;
 	private RecyclerView.Adapter mAdapter;
 	private RecyclerView.LayoutManager layoutManager;
 	private WeatherCondition[] forecastedConditions = new WeatherCondition[NUMBER_OF_DAYS];
-	private JSONObject lastForcast;
+	private JSONObject lastForecast;
+	private Gson gson = new Gson();
+	private boolean loadPrevious;
+	File dataPath, dataFile;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -56,14 +93,18 @@ public class SecondPageActivity extends AppCompatActivity {
 		info.setVisibility(View.INVISIBLE);
 		Intent intent = getIntent();
 		String text = intent.getStringExtra("XY");
-		String[] parts = text.split(",");
-		longitude = parts[0].substring(1);
-		latitude = parts[1].substring(0, parts[1].length() - 1);
-		mQueue = Volley.newRequestQueue(this);
+		loadPrevious = intent.getBooleanExtra("loadPrevious", false);
+		if (!loadPrevious) {
+			String[] parts = text.split(",");
+			longitude = parts[0].substring(1);
+			latitude = parts[1].substring(0, parts[1].length() - 1);
+			mQueue = Volley.newRequestQueue(this);
+		}
 		recyclerView = findViewById(R.id.forecastRecyclerView);
 		layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
 		recyclerView.setLayoutManager(layoutManager);
-
+		dataPath = getApplicationContext().getFilesDir();
+		dataFile = new File(dataPath, getResources().getString(R.string.data_json_url));
 		WeatherRunnable weatherRunnable = new WeatherRunnable();
 		Thread weatherThread = new Thread(weatherRunnable);
 		weatherThread.start();
@@ -72,6 +113,61 @@ public class SecondPageActivity extends AppCompatActivity {
 	class WeatherRunnable implements Runnable {
 		@Override
 		public void run() {
+			if (!loadPrevious)
+				sendRequest();
+			else
+				loadPreviousData();
+		}
+
+		private void loadPreviousData() {
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(dataFile));
+				WeatherData data = gson.fromJson(reader, WeatherData.class);
+				currentCondition = data.getCurrentCondition();
+				forecastedConditions = data.getForecastedConditions();
+				name = data.getName();
+				region = data.getRegion();
+				country = data.getCountry();
+				render();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+
+		void render()
+		{
+			mainHandler.post(new Runnable() {
+				@Override
+				public void run() {
+					TextView cityName = findViewById(R.id.cityName);
+					cityName.setText(String.format("%s, %s, %s", name, region, country));
+					ProgressBar pb = findViewById(R.id.progressBar);
+					pb.setVisibility(View.INVISIBLE);
+					ConstraintLayout info = findViewById(R.id.weather_info);
+					TextView tempC = findViewById(R.id.tempC);
+					TextView tempF = findViewById(R.id.tempF);
+					tempC.setText(Long.toString(Math.round(currentCondition.getTempC())));
+					tempF.setText(Long.toString(Math.round(currentCondition.getTempF())));
+					ImageView conditionImage = findViewById(R.id.conditionImage);
+					String imageName = currentCondition.getImageName();
+					conditionImage.setImageResource(conditionImage.getContext().getResources()
+							.getIdentifier(imageName, "drawable", conditionImage.getContext().getPackageName()));
+					TextView condition = findViewById(R.id.condition);
+					condition.setText(currentCondition.getConditionText());
+					TextView wind = findViewById(R.id.wind);
+					wind.setText(String.format("%.1f km/h", currentCondition.getWindKph()));
+					TextView humidity = findViewById(R.id.humidity);
+					humidity.setText(String.format("%d%%", currentCondition.getHumidity()));
+					TextView pressure = findViewById(R.id.pressure);
+					pressure.setText(String.format("%.1f mb", currentCondition.getPressureMb()));
+					mAdapter = new WeatherAdapter(forecastedConditions);
+					recyclerView.setAdapter(mAdapter);
+					info.setVisibility(View.VISIBLE);
+				}
+			});
+		}
+		void sendRequest()
+		{
 			String json_url = getString(R.string.weather_api_url, latitude, longitude);
 			JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, json_url, null, new Response.Listener<JSONObject>() {
 				@Override
@@ -87,7 +183,7 @@ public class SecondPageActivity extends AppCompatActivity {
 								.withIsDay(currentTemp.getInt("is_day") != 0)
 								.withConditionText(currentTemp.getJSONObject("condition").getString("text"))
 								.withConditionCode(Integer.parseInt(WeatherCondition.getIconName(currentTemp.getJSONObject("condition")
-									.getString("icon"))))
+										.getString("icon"))))
 								.withWindKph(currentTemp.getDouble("wind_kph"))
 								.withPressureMb(currentTemp.getDouble("pressure_mb"))
 								.withPrecipMm(currentTemp.getDouble("precip_mm"))
@@ -98,7 +194,7 @@ public class SecondPageActivity extends AppCompatActivity {
 						JSONArray forecasts = response.getJSONObject("forecast").getJSONArray("forecastday");
 						for (int i = 0; i < forecasts.length(); i++) {
 							JSONObject forecast = forecasts.getJSONObject(i);
-							lastForcast = forecast;
+							lastForecast = forecast;
 							JSONObject weather = forecast.getJSONObject("day");
 							forecastedConditions[i] = new WeatherCondition.Builder()
 									.withTimestamp(forecast.getLong("date_epoch"))
@@ -113,57 +209,14 @@ public class SecondPageActivity extends AppCompatActivity {
 									.withConditionText(weather.getJSONObject("condition").getString("text"))
 									.build();
 						}
-
-//						//writing the file
-//
-//						FileOutputStream fos = null;
-//						try {
-//							fos = openFileOutput("myJSON.json", MODE_PRIVATE);
-//							fos.write(lastForcast.toString().getBytes());
-//						} catch (FileNotFoundException e) {
-//							e.printStackTrace();
-//						} catch (IOException e) {
-//							e.printStackTrace();
-//						} finally {
-//							if (fos != null) {
-//								try {
-//									fos.close();
-//								} catch (IOException e) {
-//									e.printStackTrace();
-//								}
-//							}
-//						}
-
-						mainHandler.post(new Runnable() {
-							@Override
-							public void run() {
-								TextView cityName = findViewById(R.id.cityName);
-								cityName.setText(String.format("%s, %s, %s", name, region, country));
-								ProgressBar pb = findViewById(R.id.progressBar);
-								pb.setVisibility(View.INVISIBLE);
-								ConstraintLayout info = findViewById(R.id.weather_info);
-								TextView tempC = findViewById(R.id.tempC);
-								TextView tempF = findViewById(R.id.tempF);
-								tempC.setText(Long.toString(Math.round(currentCondition.getTempC())));
-								tempF.setText(Long.toString(Math.round(currentCondition.getTempF())));
-								ImageView conditionImage = findViewById(R.id.conditionImage);
-								String imageName = currentCondition.getImageName();
-                                conditionImage.setImageResource(conditionImage.getContext().getResources()
-                                        .getIdentifier(imageName, "drawable", conditionImage.getContext().getPackageName()));
-								TextView condition = findViewById(R.id.condition);
-								condition.setText(currentCondition.getConditionText());
-								TextView wind = findViewById(R.id.wind);
-								wind.setText(String.format("%.1f km/h", currentCondition.getWindKph()));
-								TextView humidity = findViewById(R.id.humidity);
-								humidity.setText(String.format("%d%%", currentCondition.getHumidity()));
-								TextView pressure = findViewById(R.id.pressure);
-								pressure.setText(String.format("%.1f mb", currentCondition.getPressureMb()));
-								mAdapter = new WeatherAdapter(forecastedConditions);
-								recyclerView.setAdapter(mAdapter);
-                                info.setVisibility(View.VISIBLE);
-							}
-						});
-					} catch (JSONException e) {
+						// Writing the file
+						WeatherData data = new WeatherData(name, region, country, currentCondition, forecastedConditions);
+						Writer writer = new FileWriter(dataFile);
+						gson.toJson(data, writer);
+						writer.flush();
+						writer.close();
+						render();
+					} catch (JSONException | IOException e) {
 						e.printStackTrace();
 					}
 				}
@@ -174,7 +227,6 @@ public class SecondPageActivity extends AppCompatActivity {
 				}
 			});
 			mQueue.add(jsonObjectRequest);
-
 		}
 	}
 }
